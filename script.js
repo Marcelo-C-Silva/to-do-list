447cb2cc4bb18127a6b484da17420f7591b656d1
Marcelo <joao69@aluno.senai-ce.org.br>
2023-08-23 09:20:45 -0300

    mostrando task na tela

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
 let button = document.querySelector('.button-add-task');
 let input = document.querySelector('.input-task');
+let listaCompleta = document.querySelector('.list-task');
 
 let minhaListaDeItens = []
 
 function adicionarNovaTarefa() {
     minhaListaDeItens.push(input.value);
-    console.log(minhaListaDeItens)
+    mostrarTarefa();
+}
+
+function mostrarTarefa() {
+
+    let novaLi = '';
+    minhaListaDeItens.forEach((tarefa) => {
+        novaLi = novaLi + ` 
+        <li class="task">
+        <img src="./img/checked.png" alt="">
+        <p>${tarefa}</p>
+        <img src="./img/trash.png" alt="">
+        </li>
+    `
+    });
+
+    listaCompleta.innerHTML = novaLi;
 }
 button.addEventListener('click', adicionarNovaTarefa);

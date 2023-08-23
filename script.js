28a8bc5bce6ff06539467e2b6c1eef9e9ee54f42
Marcelo <joao69@aluno.senai-ce.org.br>
2023-08-23 19:42:53 -0300

    deletar itens

--- script.js
+++ script.js
@@ -12,16 +12,22 @@ function adicionarNovaTarefa() {
 function mostrarTarefa() {
 
     let novaLi = '';
-    minhaListaDeItens.forEach((tarefa) => {
+    minhaListaDeItens.forEach((tarefa, posicao) => {
         novaLi = novaLi + ` 
         <li class="task">
         <img src="./img/checked.png" alt="">
         <p>${tarefa}</p>
-        <img src="./img/trash.png" alt="">
+        <img src="./img/trash.png" alt="" onclick ='deletarItem(${posicao})'>
         </li>
     `
     });
 
     listaCompleta.innerHTML = novaLi;
 }
+
+function deletarItem(posicao) {
+    minhaListaDeItens.splice(posicao, 1);
+    mostrarTarefa();
+}
+
 button.addEventListener('click', adicionarNovaTarefa);

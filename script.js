2a5abd79b255eeb83436239f0e5d8d0665ac8634
Marcelo <joao69@aluno.senai-ce.org.br>
2023-08-23 08:51:59 -0300

    criando função para adicionar nova tarefa

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
-let button = document.querySelector('.button-add-task')
-let input = document.querySelector('.input-task')
+let button = document.querySelector('.button-add-task');
+let input = document.querySelector('.input-task');
 
+let minhaListaDeItens = []
+
+function adicionarNovaTarefa() {
+    minhaListaDeItens.push(input.value);
+    console.log(minhaListaDeItens)
+}
+button.addEventListener('click', adicionarNovaTarefa);
